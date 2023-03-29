937f26225817d623d8d5d08a689645e7f54217c4
rinktvn2525 <rinktvn2525@gmail.com>
2023-03-29 22:55:31 +0700

    fix update method recipe

--- src/Pages/Recipe/Recipe.js
+++ src/Pages/Recipe/Recipe.js
@@ -15,64 +15,37 @@ const cx = classNames.bind(styles);
 function Recipe() {
     const [recipes, setRecipes] = useState([]);
     const [menu, setMenu] = useState([]);
-    const [editBF, setEditBF] = useState(false);
-    const [checkedCount, setCheckedCount] = useState(0);
-    const [checkedItems, setCheckedItems] = useState([]);
+    const getRecipesData = async () => {
+        const token = localStorage.getItem('token');
+        const results = await recipeService.getRecipe(token);
+        setRecipes(results);
+    };
+    const getMenuData = async () => {
+        const token = localStorage.getItem('token');
+        const results = await recipeService.getMenu(token);
+        setMenu(results);
+    };
     useEffect(() => {
-        const getRecipesData = async () => {
-            const token = localStorage.getItem('token');
-            const results = await recipeService.getRecipe(token);
-            setRecipes(results);
-        };
         getRecipesData();
-    }, [editBF]);
+    }, []);
     useEffect(() => {
-        const getMenuData = async () => {
-            const token = localStorage.getItem('token');
-            const results = await recipeService.getMenu(token);
-            setMenu(results);
-        };
         getMenuData();
     }, []);
-    const onEditing = () => {
-        if (!editBF) {
-            setEditBF(true);
-            menu.forEach((item) => {
-                if (isSelected(1, item)) {
-                    setCheckedCount((prev) => prev + 1);
-                    item.type = 1;
-                    setCheckedItems((prev) => [...prev, item]);
-                }
-            });
-        }
-    };
-    const isSelected = (type, item) => {
-        return recipes.some((recipe) => recipe.type === type && recipe.id === item.id);
-    };
-    const handleCheckboxChange = (e, data) => {
-        if (e.target.checked) {
-            setCheckedCount(checkedCount + 1);
-            data.type = 1;
-            setCheckedItems((prev) => [...prev, data]);
-        } else if (!e.target.checked) {
-            setCheckedCount(checkedCount - 1);
-            const newItems = checkedItems.filter((item) => item.id !== data.id);
-            setCheckedItems(newItems);
-        }
-    };
-    const handleSumitUpdate = async (type) => {
-        const newRecipes = recipes.filter((item) => item.type !== type).concat(checkedItems);
-
-        console.log(newRecipes);
+    const handleSubmitEdit = (type) => async (checkedItems) => {
+        const newRecipes = recipes
+            .filter((item) => item.type !== type)
+            .concat(
+                checkedItems.map((recipe) => {
+                    return { ...recipe, type: type };
+                }),
+            );
+        setRecipes(newRecipes);
         const token = localStorage.getItem('token');
-        // fixx put method
-        // await recipeService.deleteRecipe(token);
+        console.log(recipes);
         const results = await recipeService.updateRecipe(newRecipes, token);
         console.log(results);
-        setEditBF(false);
-        setCheckedCount(0);
-        setCheckedItems([]);
     };
+
     return (
         <Card className={cx('wrapper')}>
             <div className={cx('header')}>
@@ -82,73 +55,31 @@ function Recipe() {
                 <Calendar />
             </div>
             <div className={cx('body')}>
-                <RecipeList edit onClickEdit={onEditing} title="Breakfast" subtitle="200kcal">
-                    {editBF ? (
-                        <div>
-                            <div className={cx('menu-list')}>
-                                {menu.map((item, index) => {
-                                    return (
-                                        <div key={index}>
-                                            <RecipeItem
-                                                data={item}
-                                                editing
-                                                onChangeEditing={handleCheckboxChange}
-                                                disableInput={checkedCount >= 3}
-                                                selected={isSelected(1, item)}
-                                            />
-                                        </div>
-                                    );
-                                })}
-                            </div>
-                            <div className={cx('menu-btn')}>
-                                <Button
-                                    onClick={() => {
-                                        setEditBF(false);
-                                        setCheckedCount(0);
-                                        setCheckedItems([]);
-                                    }}
-                                    className={cx('confirm-btn')}
-                                >
-                                    Cancel
-                                </Button>
-                                <Button onClick={() => handleSumitUpdate(1)} primary className={cx('confirm-btn')}>
-                                    Confirm
-                                </Button>
-                            </div>
-                        </div>
-                    ) : (
-                        recipes.map((recipe, index) => {
-                            if (recipe.type === 1) {
-                                return <RecipeItem key={index} data={recipe} />;
-                            }
-                            return '';
-                        })
-                    )}
-                </RecipeList>
-                <RecipeList edit title="Lunch" subtitle="500kcal">
-                    {recipes.map((recipe, index) => {
-                        if (recipe.type === 2) {
-                            return <RecipeItem key={index} data={recipe} />;
-                        }
-                        return '';
-                    })}
-                </RecipeList>
-                <RecipeList edit title="Supper" subtitle="300kcal">
-                    {recipes.map((recipe, index) => {
-                        if (recipe.type === 3) {
-                            return <RecipeItem key={index} data={recipe} />;
-                        }
-                        return '';
-                    })}
-                </RecipeList>
-                <RecipeList edit title="Dinner" subtitle="400kcal">
-                    {recipes.map((recipe, index) => {
-                        if (recipe.type === 4) {
-                            return <RecipeItem key={index} data={recipe} />;
-                        }
-                        return '';
-                    })}
-                </RecipeList>
+                <RecipeList
+                    menuData={menu}
+                    onEditDone={handleSubmitEdit(1)}
+                    edit
+                    listData={recipes.filter((recipe) => recipe.type === 1)}
+                    title="Breakfast"
+                    subtitle="200kcal"
+                />
+                <RecipeList
+                    menuData={menu}
+                    onEditDone={handleSubmitEdit(2)}
+                    edit
+                    listData={recipes.filter((recipe) => recipe.type === 2)}
+                    title="Lunch"
+                    subtitle="500kcal"
+                />
+
+                <RecipeList
+                    menuData={menu}
+                    onEditDone={handleSubmitEdit(3)}
+                    edit
+                    listData={recipes.filter((recipe) => recipe.type === 3)}
+                    title="Dinner"
+                    subtitle="400kcal"
+                />
             </div>
         </Card>
     );
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,9 +1,45 @@
 import styles from './List.module.scss';
 import classNames from 'classnames/bind';
 import { RiEditFill } from 'react-icons/ri';
+import RecipeItem from '../Item/Item';
+import { useState } from 'react';
+import Button from '../Button/Button';
 
 const cx = classNames.bind(styles);
-function List({ title, className, subtitle, edit = false, onClickEdit, children }) {
+function List({ title, subtitle, edit = false, listData, menuData, onEditDone, children, className }) {
+    const [checkedCount, setCheckedCount] = useState(0);
+    const [checkedItems, setCheckedItems] = useState([]);
+    const [showEdit, setShowEdit] = useState(false);
+    const isSelected = (compareItem) => {
+        return listData.some((item) => item.id === compareItem.id);
+    };
+    const onClickEdit = () => {
+        if (!showEdit) {
+            setShowEdit(true);
+            menuData.forEach((item) => {
+                if (isSelected(item)) {
+                    setCheckedCount((prev) => prev + 1);
+                    setCheckedItems((prev) => [...prev, item]);
+                }
+            });
+        }
+    };
+    const handleCheckboxChange = (e, data) => {
+        if (e.target.checked) {
+            setCheckedCount(checkedCount + 1);
+            setCheckedItems((prev) => [...prev, data]);
+        } else if (!e.target.checked) {
+            setCheckedCount(checkedCount - 1);
+            const newItems = checkedItems.filter((item) => item.id !== data.id);
+            setCheckedItems(newItems);
+        }
+    };
+    const handleSumitUpdate = () => {
+        onEditDone(checkedItems);
+        setShowEdit(false);
+        setCheckedCount(0);
+        setCheckedItems([]);
+    };
     return (
         <div className={cx('wrapper', className)}>
             <div className={cx('header')}>
@@ -12,7 +48,44 @@ function List({ title, className, subtitle, edit = false, onClickEdit, children 
                 </h5>
                 <span className={cx('subtitle')}>{subtitle}</span>
             </div>
-            <div className={cx('body')}>{children}</div>
+            <div className={cx('body')}>
+                {showEdit ? (
+                    <div>
+                        <div className={cx('menu-list')}>
+                            {menuData.map((item, index) => {
+                                return (
+                                    <div key={index}>
+                                        <RecipeItem
+                                            data={item}
+                                            editing
+                                            onChangeEditing={handleCheckboxChange}
+                                            disableInput={checkedCount >= 3}
+                                            selected={isSelected(item)}
+                                        />
+                                    </div>
+                                );
+                            })}
+                        </div>
+                        <div className={cx('menu-btn')}>
+                            <Button
+                                onClick={() => {
+                                    setShowEdit(false);
+                                    setCheckedCount(0);
+                                    setCheckedItems([]);
+                                }}
+                                className={cx('confirm-btn')}
+                            >
+                                Cancel
+                            </Button>
+                            <Button onClick={handleSumitUpdate} primary className={cx('confirm-btn')}>
+                                Confirm
+                            </Button>
+                        </div>
+                    </div>
+                ) : (
+                    listData.map((recipe, index) => <RecipeItem key={index} data={recipe} />)
+                )}
+            </div>
         </div>
     );
 }
